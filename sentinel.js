6602305759508f8619166d76df9be2e5ad7ad9ef
Kartik Rao <kartikeya.rao@gmail.com>
2011-04-07 17:15:16 +1000

    Comments cleanup

--- sentinel.js
+++ sentinel.js
@@ -11,6 +11,8 @@
 (function () {
 	window.sentinel = {};
 	
+	/* Event handling code is based on Dean Edward's event js*/
+	
 	/**
 	 * Attach an event to the given element 
 	 * 
@@ -20,26 +22,19 @@
 	 * @param {function} handler Event handler
 	 */
 	var addEvent = function (element, type, handler) {
-  	  // assign each event handler a unique ID
   	  if (!handler.$$guid) handler.$$guid = addEvent.guid++;
-  	  // create a hash table of event types for the element
   	  if (!element.events) element.events = {};
-  	  // create a hash table of event handlers for each element/event pair
   	  var handlers = element.events[type];
   	  if (!handlers) {
   	    handlers = element.events[type] = {};
-  	    // store the existing event handler (if there is one)
   	    if (element["on" + type]) {
   	      handlers[0] = element["on" + type];
   	    }
   	  }
-  	  // store the event handler in the hash table
   	  handlers[handler.$$guid] = handler;
-  	  // assign a global event handler to do all the work
   	  element["on" + type] = handleEvent;
   	};
 
-  	// a counter used to create unique IDs
   	addEvent.guid = 1;
 
 	/**
@@ -51,7 +46,6 @@
 	 * @param {function} handler Event handler
 	 */
   	var removeEvent = function (element, type, handler) {
-  	  // delete the event handler from the hash table
   	  if (element.events && element.events[type]) {
   	    delete element.events[type][handler.$$guid];
   	  }
@@ -64,11 +58,8 @@
 	 * @param {object} event The event to handle
 	 */
   	var handleEvent = function (event) {
-  	  // grab the event object (IE uses a global event object)
   	  event = event || window.event;
-  	  // get a reference to the hash table of event handlers
   	  var handlers = this.events[event.type];
-  	  // execute each event handler
   	  for (var i in handlers) {
   	    this.$$handleEvent = handlers[i];
   	    this.$$handleEvent(event);
@@ -130,6 +121,14 @@
       }
     };
     
+	/**
+	 * Watch an element, execute the callback when it is within px distance
+	 * 
+	 * @name watchElement
+	 * @param {DOMElement} el The element to watch
+	 * @param {function} fn The callback to execute
+	 * @param {number} px Execute callback when user is px distance away from scrolling into the element
+	 */
     sentinel.watchElement = function(el, fn, px) {
   	  if (elementInViewport(el, px || config.defaultDistance)) {
   		  try {
@@ -146,6 +145,13 @@
 	  }
 	};
     
+	/**
+	 * Unwatch the element
+	 * 
+	 * @private
+	 * @name unwatchElement
+	 * @param {string} hash Unique tracking id of the element
+	 */
 	sentinel.unwatchElement = function(hash) {
     	var item = stack[hash];
     	if(item) {
@@ -157,6 +163,12 @@
     	}
 	};
 	
+	/**
+	 * Initialises sentinel
+	 * 
+	 * @name init
+	 * @param {object} user Object with distance parameter
+	 */
 	sentinel.init = function(user) {
       if(user && typeof user.distance == "number") {
     	  config.defaultDistance = user.distance; 
@@ -165,6 +177,4 @@
       addEvent(window, CONSTANTS.SCROLL, handler);
       config.init = true;
 	};
-	
-	
 })();  	
